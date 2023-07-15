e4d2d841af491f120adb6f2a9579ed9e0a110d68
Nikita Malienko <nikita@eltexsoft.com>
2023-07-15 23:04:37 +0300

    add keyboard layout

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,7 @@
 import { WORDS, KEYBOARD_LETTERS } from './consts';
 
 const gameDiv = document.querySelector('#game');
+const logoH1 = document.querySelector('#logo');
 
 const createPlaceholdersHTML = () => {
   const word = sessionStorage.getItem('word');
@@ -13,11 +14,49 @@ const createPlaceholdersHTML = () => {
   return `<div id="placeholders" class="placeholders-wrapper">${placeHoldersHTML}</div>`;
 };
 
+const createKeyboard = () => {
+  const keyboard = document.createElement('div');
+  keyboard.classList.add('keyboard');
+  keyboard.id = 'keyboard';
+
+  const keyboardHTML = KEYBOARD_LETTERS.reduce((acc, curr) => {
+    return (
+      acc +
+      `<button class="button-primary keyboard-button" id="${curr}">${curr}</button>`
+    );
+  }, '');
+
+  keyboard.innerHTML = keyboardHTML;
+  return keyboard;
+};
+
+const createHangmanImg = () => {
+  const image = document.createElement('img');
+  image.src = 'images/hg-0.png';
+  image.alt = 'hangman image';
+  image.classList.add('hangman-img');
+  image.id = 'hangman-img';
+
+  return image;
+};
+
 export const startGame = () => {
+  logoH1.classList.add('logo-sm');
   const randomIndex = Math.floor(Math.random() * WORDS.length);
   const wordToGuess = WORDS[randomIndex];
   sessionStorage.setItem('word', wordToGuess);
-  gameDiv.innerHTML = createPlaceholdersHTML();
-};
 
-console.log(KEYBOARD_LETTERS);
+  gameDiv.innerHTML = createPlaceholdersHTML();
+
+  gameDiv.innerHTML +=
+    '<p id="tries" class="mt-2">TRIES LEFT: <span id="tries-left" class="font-medium text-red-600">10</span></p>';
+
+  const keyboardDiv = createKeyboard();
+  keyboardDiv.addEventListener('click', (event) => {
+    console.log(event.target.id);
+  });
+
+  const hangmanImg = createHangmanImg();
+  gameDiv.prepend(hangmanImg);
+  gameDiv.appendChild(keyboardDiv);
+};
