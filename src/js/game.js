f25e5a4b39e0a1fe7dcd342c24bb95f02143ce10
Nikita Malienko <nikita@eltexsoft.com>
2023-07-16 16:03:03 +0300

    fix keyboard click event, add input letter handler function, disable double click letter

--- src/js/game.js
+++ src/js/game.js
@@ -3,6 +3,8 @@ import { WORDS, KEYBOARD_LETTERS } from './consts';
 const gameDiv = document.querySelector('#game');
 const logoH1 = document.querySelector('#logo');
 
+let triesLeft;
+
 const createPlaceholdersHTML = () => {
   const word = sessionStorage.getItem('word');
   const placeHoldersHTML = word
@@ -40,7 +42,31 @@ const createHangmanImg = () => {
   return image;
 };
 
+const checkLetter = (letter) => {
+  const word = sessionStorage.getItem('word');
+  const inputLetter = letter.toLowerCase();
+
+  if (!word.includes(inputLetter)) {
+    const triesCounter = document.querySelector('#tries-left');
+    triesLeft -= 1;
+    triesCounter.innerText = triesLeft;
+
+    const hangmanImg = document.querySelector('#hangman-img');
+    hangmanImg.src = `images/hg-${10 - triesLeft}.png`;
+  } else {
+    const wordArray = word.split('');
+    wordArray.forEach((currentLetter, index) => {
+      if (currentLetter === inputLetter) {
+        document.querySelector(`#letter_${index}`).innerText =
+          inputLetter.toUpperCase();
+      }
+    });
+  }
+};
+
 export const startGame = () => {
+  triesLeft = 10;
+
   logoH1.classList.add('logo-sm');
   const randomIndex = Math.floor(Math.random() * WORDS.length);
   const wordToGuess = WORDS[randomIndex];
@@ -48,12 +74,14 @@ export const startGame = () => {
 
   gameDiv.innerHTML = createPlaceholdersHTML();
 
-  gameDiv.innerHTML +=
-    '<p id="tries" class="mt-2">TRIES LEFT: <span id="tries-left" class="font-medium text-red-600">10</span></p>';
+  gameDiv.innerHTML += `<p id="tries" class="mt-2">TRIES LEFT: <span id="tries-left" class="font-medium text-red-600">${triesLeft}</span></p>`;
 
   const keyboardDiv = createKeyboard();
   keyboardDiv.addEventListener('click', (event) => {
-    console.log(event.target.id);
+    if (event.target.tagName.toLowerCase() === 'button') {
+      event.target.disabled = true;
+      checkLetter(event.target.id);
+    }
   });
 
   const hangmanImg = createHangmanImg();
